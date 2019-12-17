89aa2d0f4397a3b43b8dd97d70d9593928c507c9
Jonas Freire <jonasfreireperson@gmail.com>
2019-12-17 19:27:30 -0300

    Criacao de animacoes no tabuleiro apos clique do mouse

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -3,8 +3,8 @@ const tic_tac_toe = {
     symbols: {
       options: ['X',"O"],
       turn_index: 0,
-      change: function(){
-          this.turn_index = (this.turn_index === 0 ? 1:0)
+      changeTurn() {
+          this.turn_index = (this.turn_index + 1) % 2
       }
     },
     container_element: null,
@@ -20,73 +20,79 @@ const tic_tac_toe = {
         [2,4,6]
     ],
 
-    init: function(container){
+    init(container) {
         this.container_element = container
     },
 
-    make_play: function(position){
-        if(this.gameover) return false
-        if(this.board[position] == ''){
-            this.board[position] = this.symbols.options [ this.symbols.turn_index ]
-            this.draw()
-            let winnig_sequence_index = this.check_winning_sequences(this.symbols.options [ this.symbols.turn_index ])
+    write_symbol(position) {
+        this.board[position] = this.symbols.options[ this.symbols.turn_index ];
+        
+        element = this.container_element.querySelector(`div:nth-child(${position+1})`);
+
+        element.classList.add('position-clicked');
+        element.innerHTML = this.board[position];
+    },
+
+    make_play(position) {
+        if (this.gameover) return false
+        if (this.board[position] == '') {
+            this.write_symbol(position)
+            let winnig_sequence_index = this.check_winning_sequences(this.symbols.options[ this.symbols.turn_index ])
             if(winnig_sequence_index >= 0 ){
                 this.game_is_over();
                 this.stylize_winner_sequence(this.winning_sequences[winnig_sequence_index])
-                alert(`O jogo acabou!`)
+                // alert(`O jogo acabou!`)
                 
-            } else if(!this.board.includes('')){
+            } else if (!this.board.includes('')) {
                 this.game_is_over();
-                alert(`Deu VELHA, jogo empatado!`)
-            }else{
-            this.symbols.change();
+                // alert(`Deu VELHA, jogo empatado!`)
+            } else {
+                this.symbols.changeTurn();
             }
             return true
-        } else{
+        } else {
             return false
         }
     },
 
-    stylize_winner_sequence(winning_sequences){
+    stylize_winner_sequence(winning_sequences) {
         winning_sequences.forEach((position) => {
             this
             .container_element
             .querySelector(`div:nth-child(${position + 1})`)
             .classList.add('winner')
-            
         })
-        
     },
 
-    game_is_over: function(){
+    game_is_over() {
         this.gameover = true
         console.log("GAME OVER")
     },
 
-    start: function(){
+    start() {
         this.board.fill('')
-        this.draw()
+        this.drawCleanBoard()
         this.gameover = false;
     },
 
-    check_winning_sequences: function(symbols){
-        for(i in this.winning_sequences){
-            if(this.board[this.winning_sequences[i][0]] == symbols &&
+    check_winning_sequences(symbols) {
+        for (i in this.winning_sequences){
+            if (this.board[this.winning_sequences[i][0]] == symbols &&
                 this.board[this.winning_sequences[i][1]] == symbols &&
-                this.board[this.winning_sequences[i][2]] == symbols){
+                this.board[this.winning_sequences[i][2]] == symbols) {
                     console.log('Sequencia vencedora: '+ i)
                     return i
-                    
-                }
+            }
         }
+
         return -1;
     },
 
-    draw: function(){
+    drawCleanBoard() {
         let content = ''
-
-        for(i in this.board){
-            content += '<div onclick="tic_tac_toe.make_play('+i+')">'+this.board[i]+'</div>'
+        
+        for (i in this.board) {
+            content += `<div onclick="tic_tac_toe.make_play(${i})">${this.board[i]}</div>`
         }
 
         this.container_element.innerHTML = content
